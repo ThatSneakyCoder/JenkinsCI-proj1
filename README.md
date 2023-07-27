f0f96308adf454ba3ac25690618fa0202bbff4fc
Shubh Srivastava <shubh22.srivastava@gmail.com>
2023-07-27 21:00:21 +0530

    Commit 4: Final Beta application

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple terminal Java Application
+
+This terminal java application tells dad jokes and also prints the message
+you have written.
--- src/main/java/com/SuperCalculator/CalcProgram/driver/App.java
+++ src/main/java/com/SuperCalculator/CalcProgram/driver/App.java
@@ -1,4 +1,11 @@
 package com.SuperCalculator.CalcProgram.driver;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 /**
  * Class containing terminal application
@@ -6,11 +13,11 @@ package com.SuperCalculator.CalcProgram.driver;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        if (args.length < 2) {
-            System.out.println("Insufficient parameters. Please refer below for how to use the command line tool");
-            System.exit(1);
+        if (args.length < 1) {
+            System.out.println("Insufficient parameters");
+            System.exit(0);
         }
 
         if ("--show-message".equals(args[0])) {
@@ -20,6 +27,16 @@ public class App
                 System.out.println("Empty message :O");
                 System.exit(1);
             }
+        } else if ("--tell-dad-joke".equals(args[0])) {
+            URL url = new URL("https://icanhazdadjoke.com/");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("Accept", "application/json");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = reader.readLine();
+            reader.close();
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(line);
+            System.out.println(root.path("joke").asText());
         } else {
             System.out.println("Incorrect parameters. Please refer below for how to use the command line tool");
             System.exit(1);
